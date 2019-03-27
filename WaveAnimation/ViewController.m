4cd5aacfdf332ed6c9d73c6d0a576ab514eac0bb
guoliting <guoliting@didichuxing.com>
2019-03-27 11:43:19 +0800

    增加YSWaterWaveView，显示渐变效果。

--- WaveAnimation/ViewController.m
+++ WaveAnimation/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "WaveView.h"
+#import "YSWaterWaveView.h"
 
 @interface ViewController ()
 
@@ -19,9 +20,19 @@
     [super viewDidLoad];
     
     CGFloat originX = 20;
-    WaveView *waveView = [[WaveView alloc] initWithFrame:CGRectMake(originX, (self.view.frame.size.height-self.view.frame.size.width/2)/2, self.view.frame.size.width - 2 * originX, self.view.frame.size.width/2)];
+    CGFloat originY = 20 + 44 + 20;
+    WaveView *waveView = [[WaveView alloc] initWithFrame:CGRectMake(originX, originY, self.view.frame.size.width - 2 * originX, self.view.frame.size.width/2)];
     waveView.layer.cornerRadius = 8;
     [self.view addSubview:waveView];
+    
+    CGRect frame = waveView.frame;
+    frame.origin.y += frame.size.height + 50;
+    YSWaterWaveView *waterWaveView = [[YSWaterWaveView alloc] initWithFrame:frame];
+    waterWaveView.layer.cornerRadius = 8;
+    waterWaveView.clipsToBounds = YES;
+    [self.view addSubview:waterWaveView];
+    [waterWaveView startWaveToPercent:0.3];
+//    [waterWaveView setGrowthSpeed:0.1];
 }
 
 @end
