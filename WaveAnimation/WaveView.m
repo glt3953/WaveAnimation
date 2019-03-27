d4fc6cc99836976e94bd7d9d6328f3cac6c52fb4
guoliting <guoliting@didichuxing.com>
2019-03-27 11:14:19 +0800

    增加“渐变效果测试代码”

--- WaveAnimation/WaveView.m
+++ WaveAnimation/WaveView.m
@@ -15,6 +15,7 @@
 @property (nonatomic, strong) CAShapeLayer *firstWaveLayer; //一个本身没有形状的图层，他的形状来源于你给定的Path，它依附于Path
 @property (nonatomic, strong) CAShapeLayer *secondWaveLayer;
 @property (nonatomic, strong) CAShapeLayer *thirdWaveLayer;
+@property (nonatomic, strong) CAGradientLayer *thirdGradientLayer;   // 绘制渐变
 @property (nonatomic, copy) NSArray *wavesArray;
 
 @end
@@ -94,6 +95,7 @@
     _thirdWaveLayer.fillColor = [UIColor colorFromHexString:@"#4DA6FF" alpha:0.6].CGColor;
     //添加到view的layer上
     [self.layer addSublayer:_thirdWaveLayer];
+//    [self setupThirdGradientLayer];
     //设置波纹流动速度
     waveSpeedC = 0.2;
     //设置波纹振幅
@@ -107,6 +109,38 @@
     _waveDisplaylink = [CADisplayLink displayLinkWithTarget:self selector:@selector(getCurrentWave:)];
     _waveDisplaylink.frameInterval = 2;//设置定时器刷新的频率，屏幕刷新两帧定时器才会触发一次（iOS设备的默认刷新频率是60HZ也就是60帧，即每秒刷新60次）
     [_waveDisplaylink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];//添加到RunLoop中
+}
+
+- (void)setupThirdGradientLayer {
+    /*
+     *初始化第三个渐变
+     */
+    _thirdGradientLayer = [CAGradientLayer layer];
+    // gradientLayer在上升完成之后的frame值，如果gradientLayer在上升过程中不断变化frame值会导致一开始绘制卡顿，所以只进行一次赋值
+    //    CGFloat gradientLayerHeight = waveAmplitudeC * sin(waveCycle * 1+ offsetXC)+currentK;
+    //    _thirdGradientLayer.frame = CGRectMake(0, CGRectGetHeight(self.frame) - gradientLayerHeight, CGRectGetWidth(self.frame), gradientLayerHeight);
+    CGFloat gradientLayerY = currentK + 40;
+    _thirdGradientLayer.frame = CGRectMake(0, gradientLayerY, CGRectGetWidth(self.frame), CGRectGetHeight(self.frame) - gradientLayerY);
+    // 默认的渐变色
+    NSArray *colors = @[(__bridge id)[UIColor colorFromHexString:@"#4DE1FF" alpha:0.5].CGColor, (__bridge id)[UIColor colorFromHexString:@"#4DA6FF" alpha:0.1].CGColor];
+    _thirdGradientLayer.colors = colors;
+    //设定颜色分割点
+    NSInteger count = [colors count];
+    CGFloat d = 1.0 / count;
+    NSMutableArray *locations = [NSMutableArray array];
+    for (NSInteger i = 0; i < count; i++) {
+        NSNumber *num = @(d + d * i);
+        [locations addObject:num];
+    }
+    NSNumber *lastNum = @(1.0f);
+    [locations addObject:lastNum];
+    _thirdGradientLayer.locations = locations;
+    // 设置渐变方向，从上往下
+    _thirdGradientLayer.startPoint = CGPointMake(0, 0);
+    _thirdGradientLayer.endPoint = CGPointMake(0, 1);
+    [_thirdGradientLayer setMask:_thirdWaveLayer];
+    //添加到view的layer上
+    [self.layer addSublayer:_thirdGradientLayer];
 }
 
 #pragma mark - 实现波纹动画
@@ -169,54 +203,55 @@
     CGPathAddLineToPoint(pathC, nil, 0, self.frame.size.height);
     CGPathCloseSubpath(pathC);
     _thirdWaveLayer.path = pathC;
+//    _thirdGradientLayer.shadowPath = pathC;
     CGPathRelease(pathC);
     
-    /*
-     *渐变
-     */
-    //保存渐变之前的绘画状态
-    CGContextRef currentContext = UIGraphicsGetCurrentContext();
-    CGContextSaveGState(currentContext);
-    //绘制渐变剪切路径
-    UIBezierPath *path1 = [[UIBezierPath alloc] init];
-    [path1 moveToPoint:CGPointMake(100, 450)];
-    [path1 addLineToPoint:CGPointMake(250, 450)];
-    [path1 addLineToPoint:CGPointMake(250, 650)];
-    [path1 addLineToPoint:CGPointMake(100, 650)];
-    [path1 closePath];
-    //是用剪切路径剪裁图形上下文
-    [path1 addClip];
-    _thirdWaveLayer.accessibilityPath = path1;
-    //绘制渐变
-    CGFloat locations[4] = {0.0,0.4,0.7,1.0};//三个颜色节点
-    CGFloat components[16] = {1.0,0.3,0.0,1.0,//起始颜色
-        0.2,0.8,0.2,1.0,//中间颜色
-        1.0,1.0,0.5,1.0,//中间颜色
-        0.8,0.3,0.4,1.0};//终止颜色
-    //创建RGB色彩空间对象
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    CGGradientRef gradient = CGGradientCreateWithColorComponents(colorSpace, components, locations, 4);
-    /*线性渐变
-     *参数1:当前上下文
-     *参数2:渐变指针
-     *参数3，4:渐变的起始和终止位置
-     *参数5:CGGradientDrawingOptions枚举
-     typedef CF_OPTIONS (uint32_t, CGGradientDrawingOptions) {
-     kCGGradientDrawsBeforeStartLocation = (1 << 0),//扩展整个渐变到渐变的起点之前的所有点
-     kCGGradientDrawsAfterEndLocation = (1 << 1)//扩展整个渐变到渐变的终点之后的所有点
-     };
-     0表示既不往前扩展也不往后扩展
-     */
-    //渐变的起点(渐变效果在以起点和终点为轴的直线周边)
-    CGPoint startPoint = CGPointMake(100, 450);
-    //渐变的终点
-    CGPoint endPoint = CGPointMake(100, 650);
-    CGContextDrawLinearGradient(currentContext, gradient, startPoint, endPoint, kCGGradientDrawsBeforeStartLocation|kCGGradientDrawsAfterEndLocation);
-    //释放创建的C结构对象
-    CGGradientRelease(gradient);
-    CGColorSpaceRelease(colorSpace);
-    //恢复绘画状态
-    CGContextRestoreGState(currentContext);
+//    /*
+//     *渐变
+//     */
+//    //保存渐变之前的绘画状态
+//    CGContextRef currentContext = UIGraphicsGetCurrentContext();
+//    CGContextSaveGState(currentContext);
+//    //绘制渐变剪切路径
+//    UIBezierPath *path1 = [[UIBezierPath alloc] init];
+//    [path1 moveToPoint:CGPointMake(100, 450)];
+//    [path1 addLineToPoint:CGPointMake(250, 450)];
+//    [path1 addLineToPoint:CGPointMake(250, 650)];
+//    [path1 addLineToPoint:CGPointMake(100, 650)];
+//    [path1 closePath];
+//    //是用剪切路径剪裁图形上下文
+//    [path1 addClip];
+//    _thirdWaveLayer.accessibilityPath = path1;
+//    //绘制渐变
+//    CGFloat locations[4] = {0.0,0.4,0.7,1.0};//三个颜色节点
+//    CGFloat components[16] = {1.0,0.3,0.0,1.0,//起始颜色
+//        0.2,0.8,0.2,1.0,//中间颜色
+//        1.0,1.0,0.5,1.0,//中间颜色
+//        0.8,0.3,0.4,1.0};//终止颜色
+//    //创建RGB色彩空间对象
+//    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+//    CGGradientRef gradient = CGGradientCreateWithColorComponents(colorSpace, components, locations, 4);
+//    /*线性渐变
+//     *参数1:当前上下文
+//     *参数2:渐变指针
+//     *参数3，4:渐变的起始和终止位置
+//     *参数5:CGGradientDrawingOptions枚举
+//     typedef CF_OPTIONS (uint32_t, CGGradientDrawingOptions) {
+//     kCGGradientDrawsBeforeStartLocation = (1 << 0),//扩展整个渐变到渐变的起点之前的所有点
+//     kCGGradientDrawsAfterEndLocation = (1 << 1)//扩展整个渐变到渐变的终点之后的所有点
+//     };
+//     0表示既不往前扩展也不往后扩展
+//     */
+//    //渐变的起点(渐变效果在以起点和终点为轴的直线周边)
+//    CGPoint startPoint = CGPointMake(100, 450);
+//    //渐变的终点
+//    CGPoint endPoint = CGPointMake(100, 650);
+//    CGContextDrawLinearGradient(currentContext, gradient, startPoint, endPoint, kCGGradientDrawsBeforeStartLocation|kCGGradientDrawsAfterEndLocation);
+//    //释放创建的C结构对象
+//    CGGradientRelease(gradient);
+//    CGColorSpaceRelease(colorSpace);
+//    //恢复绘画状态
+//    CGContextRestoreGState(currentContext);
 }
 
 #pragma mark - 销毁定时器
